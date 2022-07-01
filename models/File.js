ed5444901d7fcd69c0bda38d6f1c531390ee9445
ak <thedeadlymortal@gmail.com>
2022-07-01 09:26:34 +0530

    #modify

---
+++ models/File.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+
+const File = mongoose.Schema({
+    path:{
+        type: String,
+        required: true
+    },
+    orgin_name: String,
+    password: String,
+    download_count:{
+        type: Number,
+        required: true,
+        default: 0
+    }
+})
+
+
+module.exports = mongoose.model("File", File)
